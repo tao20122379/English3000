0e87f41337ffb76bc7d29ea1e9e2ea2a2a9185b0
tao20122379 <anhtaotsbn@gmail.com>
2016-07-21 16:23:21 +0700

    WordDetail

---
+++ English3000/English3000/Classes/Services/WordDetailServices.swift
@@ -0,0 +1,33 @@
+//
+//  WordDetailServices.swift
+//  English3000
+//
+//  Created by khactao on 7/21/16.
+//  Copyright © 2016 khactao. All rights reserved.
+//
+
+import UIKit
+
+class WordDetailServices: NSObject {
+    
+    class func getWordDetail(word: WordModel, completionHandler: CompletionHandler) {
+        
+        let searchKey = "@"+word.name+" /"+Constants().stringReplaces(word.read!, key: ["[", "]"])+"/"
+        
+        DatabaseManager().queryDatabase("dictd_anh_viet", executyQuery: "SELECT * FROM EVDictionary WHERE meaning LIKE \"%" + searchKey + "%\"") { (status, data) in
+            
+            
+            let rs = data as! FMResultSet
+            
+            while rs.next() {
+                
+                let wordDtails: [String] = Constants().formatWordDetail(rs.stringForColumn("meaning"))
+                completionHandler(true, wordDtails)
+                
+            }
+            
+        }
+    }
+}
+
+
